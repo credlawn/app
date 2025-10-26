6468d1addaf058d2d3096486c8b06fa605f129a2
Arun Singh <admin@credlawn.com>
2025-10-26 19:01:19 +0530

    V1.7.5 Notification Final

--- lib/screens/notification_detail_screen.dart
+++ lib/screens/notification_detail_screen.dart
@@ -1,5 +1,7 @@
 
+import 'package:credlawn/custom/custom_color.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotificationDetailScreen extends StatelessWidget {
   final String title;
@@ -14,23 +16,28 @@ class NotificationDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(title),
-        backgroundColor: Colors.white,
-        elevation: 1,
-        iconTheme: const IconThemeData(color: Colors.black),
-        titleTextStyle: const TextStyle(
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
+        title: Text(title, style: GoogleFonts.poppins(color: Colors.white, fontSize: 20)),
+        backgroundColor: CustomColor.MainColor,
+        elevation: 0.5,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            body,
-            style: const TextStyle(fontSize: 16),
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          margin: const EdgeInsets.symmetric(vertical: 8.0),
+          elevation: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(color: Colors.grey.shade300, width: 1),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              body,
+              style: GoogleFonts.poppins(fontSize: 16, color: Colors.black87),
+            ),
           ),
         ),
       ),
